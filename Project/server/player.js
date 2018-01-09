9698dd2f9ade27d66e3fee6b1540104f5e7a0e74
Janneck Lange <1628574@stud.hs-mannheim.de>
2018-01-09 11:13:47 +0100

    🎨✨

--- Project/server/player.js
+++ Project/server/player.js
@@ -1,38 +1,23 @@
+const shipPlacement = require(__dirname + "/ship-placement");
+
 module.exports = class Player {
   constructor(id) {
     this._name;
-    this._ships;
     this._field;
     this._score;
     this._id = id;
-    this.reset();
+    this.init();
   }
 
   set name(name) {
     this._name = name;
   }
-
-  set ships(ships) {
-    this._ships = ships;
-  }
-
   set field(field) {
     this._field = field;
   }
 
-  addShot(x, y) {
-    this._fieldOfShots[y][x] = 1;
-  }
-
-  increaseScore() {
-    this._score++;
-  }
-
   get name() {
     return this._name;
-  }
-  get ships() {
-    return this._ships;
   }
   get field() {
     return this._field;
@@ -44,15 +29,13 @@ module.exports = class Player {
     return this._id;
   }
 
-  reset() {
-    this._ships = shipPlacement.generateShipPlacement();
-    this._field = shipLogic.addShips(this._ships);
-    this._fieldOfShots = shipLogic.createField();
-    this._score = 0;
+  increaseScore() {
+    this._score++;
   }
 
-  allShipsDestroyed() {
-    return shipLogic.allShipsDestroyed(this._ships);
+  init() {
+    this.score = 0;
+    this.field = ship - placement.randomPlayGround();
   }
 
 }
