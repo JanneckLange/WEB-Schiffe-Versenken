2d0e207ba7664a61b967d6fbf9ced78fbc26b5c1
Janneck Lange <1628574@stud.hs-mannheim.de>
2018-01-13 01:37:15 +0100

    🚑 wenn score leer wird ein neuer angelegt

--- Project/server/highscore.js
+++ Project/server/highscore.js
@@ -4,29 +4,38 @@ const highscorePath = __dirname + '/highscore.json';
 
 
 function readHighscore(path) {
-    var entries=fs.readFileSync(path, 'utf8');
+  try {
+    var entries = fs.readFileSync(path, 'utf8');
     entries = JSON.parse(entries);
     return entries;
+  } catch (e) {
+    return null;
+  }
+
 }
 
 function updateScore(score, name) {
-    let entries = readHighscore(highscorePath);
+  let entries = readHighscore(highscorePath);
+  if (!entries) {
+    entries = [];
+  }
 
-    let newEntry = {};
-    newEntry._name = name;
-    newEntry._score = score;
+  let newEntry = {};
+  newEntry._name = name;
+  newEntry._score = score;
 
-    entries.push(newEntry);
-    entries = JSON.stringify(entries);
-    try {
-        fs.writeFileSync(highscorePath, entries, 'utf8');
-        return true;
-    } catch (err) {
-        return false;
-    }
+  entries.push(newEntry);
+  entries = JSON.stringify(entries);
+  try {
+    fs.writeFileSync(highscorePath, entries, 'utf8');
+    return true;
+  } catch (err) {
+
+    return false;
+  }
 }
 
 module.exports = {
-    readHighscore: readHighscore,
-    updateScore: updateScore,
+  readHighscore: readHighscore,
+  updateScore: updateScore,
 };
